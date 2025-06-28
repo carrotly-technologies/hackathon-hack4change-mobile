c2d5bc7cf192f0e00beff723f724b17259076cbe
matjamr <jamr.mat@gmail.com>
2025-06-28 02:43:11 +0200

    Add ActivityLayout component with stack configuration
    Introduce a new layout component for the activity section using `expo-router`. It disables gestures, hides headers, and sets up a screen with a blank title.

---
+++ app/home/activity/_layout.tsx
@@ -0,0 +1,13 @@
+import {Stack} from "expo-router";
+
+const ActivityLayout = () => {
+    return <Stack
+        screenOptions={{
+            gestureEnabled: false,
+            headerShown: false,
+        }}>
+        <Stack.Screen name="index" options={{title: ''}}/>
+    </Stack>
+}
+
+export default ActivityLayout;
