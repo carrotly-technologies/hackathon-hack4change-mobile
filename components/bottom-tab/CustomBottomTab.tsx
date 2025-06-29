b377ffda440260efcb1907ce2df17cd5fd6f028a
Tomasz Kisiel <tkisiel5w4@yahoo.com>
2025-06-29 03:01:58 +0200

    fix: naive fix of first element in tab navigation

--- components/bottom-tab/CustomBottomTab.tsx
+++ components/bottom-tab/CustomBottomTab.tsx
@@ -1,24 +1,24 @@
-import React, {FC, useEffect} from 'react';
-import {StyleSheet, View} from 'react-native';
-import Animated, {runOnJS, useAnimatedProps, useSharedValue, withTiming,} from 'react-native-reanimated';
-import {interpolatePath} from 'react-native-redash';
-import Svg, {Path} from 'react-native-svg';
+import React, { FC, useEffect } from 'react';
+import { StyleSheet, View } from 'react-native';
+import Animated, { runOnJS, useAnimatedProps, useSharedValue, withTiming, } from 'react-native-reanimated';
+import { interpolatePath } from 'react-native-redash';
+import Svg, { Path } from 'react-native-svg';
 
-import {BottomTabBarProps} from '@react-navigation/bottom-tabs';
 import AnimatedCircle from "@/components/bottom-tab/AnimatedCircle";
 import TabItem from "@/components/bottom-tab/TabItem";
-import {usePathname} from "expo-router";
+import { getPathXCenter } from "@/utils/constants/Path";
+import { SCREEN_WIDTH } from "@/utils/constants/screen";
 import usePath from "@/utils/hooks/usePath";
-import {getPathXCenter} from "@/utils/constants/Path";
-import {SCREEN_WIDTH} from "@/utils/constants/screen";
+import { BottomTabBarProps } from '@react-navigation/bottom-tabs';
+import { usePathname } from "expo-router";
 
 const AnimatedPath = Animated.createAnimatedComponent(Path);
 export const CustomBottomTab: FC<BottomTabBarProps & { allItems: any[] }> = ({
-                                                                                 state,
-                                                                                 descriptors,
-                                                                                 navigation, allItems
-                                                                             }) => {
-    const {containerPath, curvedPaths, tHeight} = usePath(5);
+    state,
+    descriptors,
+    navigation, allItems
+}) => {
+    const { containerPath, curvedPaths, tHeight } = usePath(5);
     const circleXCoordinate = useSharedValue(0);
     const progress = useSharedValue(1);
     const pathname = usePathname()
@@ -26,9 +26,8 @@ export const CustomBottomTab: FC<BottomTabBarProps & { allItems: any[] }> = ({
     useEffect(() => {
         const currentIndex = state.routes
             .findIndex(route => `/home/${route.name}` === pathname);
-        if (currentIndex !== -1) {
-            progress.value = withTiming(currentIndex + 1);
-        }
+
+        progress.value = withTiming((currentIndex || 0) + 1);
     }, [pathname, state.routes]);
 
 
@@ -43,7 +42,7 @@ export const CustomBottomTab: FC<BottomTabBarProps & { allItems: any[] }> = ({
     const animatedProps = useAnimatedProps(() => {
         const currentPath = interpolatePath(
             progress.value,
-            Array.from({length: curvedPaths.length}, (_, index) => index + 1),
+            Array.from({ length: curvedPaths.length }, (_, index) => index + 1),
             curvedPaths,
         );
         runOnJS(handleMoveCircle)(currentPath);
@@ -60,9 +59,9 @@ export const CustomBottomTab: FC<BottomTabBarProps & { allItems: any[] }> = ({
     return (
         <View style={styles.tabBarContainer}>
             <Svg width={SCREEN_WIDTH} height={tHeight} style={styles.shadowMd}>
-                <AnimatedPath fill={'white'} animatedProps={animatedProps}/>
+                <AnimatedPath fill={'white'} animatedProps={animatedProps} />
             </Svg>
-            <AnimatedCircle circleX={circleXCoordinate}/>
+            <AnimatedCircle circleX={circleXCoordinate} />
             <View
                 style={[
                     styles.tabItemsContainer,
@@ -71,7 +70,8 @@ export const CustomBottomTab: FC<BottomTabBarProps & { allItems: any[] }> = ({
                     },
                 ]}>
                 {state.routes.map((route, index) => {
-                    const {options} = descriptors[route.key];
+                    const { options } = descriptors[route.key];
+
                     return (
                         <TabItem
                             key={index.toString()}
@@ -107,6 +107,11 @@ const styles = StyleSheet.create({
         shadowColor: '#000',
         shadowOpacity: 0.2,
         shadowRadius: 3,
-        shadowOffset: {width: 0, height: 3},
+        shadowOffset: { width: 0, height: 3 },
     },
 });
+
+/*
+-1 /home [{"key": "index-P8f3RTvZ6OpcUTxlyklbc", "name": "index", "params": {}, "path": undefined}, {"key": "rank-UhwJWdmnEtXol_BZ9CJ8p", "name": "rank", "params": undefined}, {"key": "activity-HC5ZEhdiBnvj0UVSmxloe", "name": "activity", "params": undefined}, {"key": "events-LuF3Q8wAEopYiYhW7kl-n", "name": "events", "params": undefined}, {"key": "profile-wlF4sqPkPppdbPioIdnX7", "name": "profile", "params": undefined}]
+-1 /home [{"key": "index-P8f3RTvZ6OpcUTxlyklbc", "name": "index", "params": {}, "path": undefined}, {"key": "rank-UhwJWdmnEtXol_BZ9CJ8p", "name": "rank", "params": undefined}, {"key": "activity-HC5ZEhdiBnvj0UVSmxloe", "name": "activity", "params": undefined}, {"key": "events-LuF3Q8wAEopYiYhW7kl-n", "name": "events", "params": undefined}, {"key": "profile-wlF4sqPkPppdbPioIdnX7", "name": "profile", "params": undefined}]
+ */
